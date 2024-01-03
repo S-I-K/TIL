616a439f03fc3563af4ec9e41bb09b1fd6b3e369
sik <daesik99@naver.com>
2024-01-03 17:21:02 +0900

    gsap: timeline(), to(), fromTo()

--- gsap/01/js/main.js
+++ gsap/01/js/main.js
@@ -1,23 +1,101 @@
-gsap.from(".title", {
-    scrollTrigger: {
-        trigger: ".full_screen",  //어느 요소에 스크롤이 도착했을 때 시작할건지
-        start: "-20% 40%",  //스크롤 트리거의 시작 기준점 설정. 앞의 값은 트리거 요소 기준(.full_screen의 상단부터 -20% 적용), 뒤의 값은 보고있는 페이지의 높이 기준.
-        end: "center center",  //스크롤 트리거의 끝점 설정.
-        scrub: .5,  //스크롤 바를 잡는데 걸리는 시간(움직임의 구현 속도 설정. 높을수록 느림.)
-        // markers: true,  //기준점을 화면에 표시할지 설정.
-        onLeave: function(){ // 끝나는지점 callback함수
-            console.log('첫번째 이벤트 완료');
+gsap.registerPlugin(ScrollTrigger);
+
+$(()=>{
+    /* section01 */
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.con01', //애니메이션이 작동되는 기준점
+            start: 'center center', // 1번째 인자: 트리거의 기준, 2번째 인자 브라우저의 기준
+            end: '60% 20%', // 위와 동일
+            scrub: 2,
+            markers: false,
         }
+    })
+    .to('h2.title', {
+        y: '140px',
+        duration: 2,
+        ease: 'ease'
+    }, 0)
+    .to('h2.title', {
+        opacity: 0,
+        duration: 2,
+        ease: 'ease'
+    }, 0)
+
+
+
+
+    /* section02 */
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.con02 ul', //애니메이션이 작동되는 기준점
+            start: 'top 100%', // 1번째 인자: 트리거의 기준, 2번째 인자 브라우저의 기준
+            end: 'center 20%', // 위와 동일
+            scrub: 2,
+            markers: false,
+        }
+    })
+    .to('.con02 li:nth-child(1)', {
+        y: '-400px',
+        duration: 1,
+        ease: 'none'
+    }, 0.2)
+    .to('.con02 li:nth-child(2)', {
+        y: '-400px',
+        duration: 1,
+        ease: 'none'
+    }, 0.4)
+    .to('.con02 li:nth-child(3)', {
+        y: '-400px',
+        duration: 1,
+        ease: 'none'
+    }, 0.6)
+    .to('.con02 li:nth-child(4)', {
+        y: '-400px',
+        duration: 1,
+        ease: 'none'
+    }, 0.8)
+
+
+
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.con03',
+            start: 'top bottom',
+            end: '30% 30%',
+            scrub: 2,
+            markers: true
+        }
+    })
+    .fromTo('.con03 span.a', {
+        y: '400%'
     },
-    scale: 10,
-});
-    gsap.from(".img01_div > img", {
-    scrollTrigger: {
-        trigger: ".img01_div",
-        start: "top center",
-        end: "bottom top",
-        scrub: 8,
-        markers: true,
+    {
+        y: '0'
+    }, 1.2)
+    .fromTo('.con03 span.b', {
+        y: '400%'
     },
-    scale: 2,
+    {
+        y: '0'
+    }, 1.4)
+    .fromTo('.con03 span.c', {
+        y: '400%'
+    },
+    {
+        y: '0'
+    }, 1.6)
+    .fromTo('.con03 span.d', {
+        y: '400%'
+    },
+    {
+        y: '0'
+    }, 1.8)
+    .fromTo('.con03 span.e', {
+        y: '400%'
+    },
+    {
+        y: '0'
+    }, 2)
 });
